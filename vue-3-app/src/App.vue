b8b45245941f769bc49a1a0c6ee1a9ea7e848dad
Aldo Silvestre <aldosilvestre89@gmail.com>
2024-05-15 13:34:05 -0300

    First commit

---
+++ vue-3-app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Aplicacion con VUE 3</h1>
+  <v-label>Componente de VUE 3</v-label>
+  <v-input type="text" />
+  <v-btn>Hola mundo</v-btn>
+  <v-btn variant="tonal">
+    Button
+  </v-btn>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-3-app/src/main.js
@@ -0,0 +1,39 @@
+import { h, createApp } from 'vue';
+import singleSpaVue from 'single-spa-vue';
+
+import App from './App.vue';
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+const vueLifecycles = singleSpaVue({
+  createApp,
+  appOptions: {
+    vuetify,
+    render() {
+      return h(App, {
+        // single-spa props are available on the "this" object. Forward them to your component as needed.
+        // https://single-spa.js.org/docs/building-applications#lifecycle-props
+        // if you uncomment these, remember to add matching prop definitions for them in your App.vue file.
+        /*
+        name: this.name,
+        mountParcel: this.mountParcel,
+        singleSpa: this.singleSpa,
+        */
+      });
+    },
+  },
+  handleInstance: (app) => { app.use(vuetify) }
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
